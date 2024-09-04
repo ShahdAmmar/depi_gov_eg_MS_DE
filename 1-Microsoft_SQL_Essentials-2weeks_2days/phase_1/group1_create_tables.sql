4fa916203f48d74a90e5926d15ab091caa806587
Shahd Ammar <shahdammar413@gmail.com>
2024-09-04 15:26:23 +0300

    create Departments & Employees tables

---
+++ 1-Microsoft_SQL_Essentials-2weeks_2days/phase_1/group1_create_tables.sql
@@ -0,0 +1,38 @@
+USE CompanyDB;
+GO
+
+/* 
+    This script sets up the Staff schema with two tables:
+        - Departments
+        - Employees
+*/
+
+CREATE TABLE Staff.Departments
+(
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(80) NOT NULL,
+    Location VARCHAR(80),
+);
+
+CREATE TABLE Staff.Employees
+(
+    EmployeeID   INT PRIMARY KEY,
+    FirstName    VARCHAR(20) NOT NULL,
+    LastName     VARCHAR(20) NOT NULL,
+    DepartmentID INT,
+    HireDate     DATE NOT NULL,
+    Position     VARCHAR(80),
+    Salary       NUMERIC(12, 3),
+    CONSTRAINT employees_chk CHECK (Salary > 0),
+    CONSTRAINT employees_chd CHECK (HireDate <= CAST(SYSDATETIME() AS DATE))
+);
+
+INSERT INTO Staff.Departments
+VALUES (1, 'Accounting & Finance', 'Cairo'),
+       (2, 'Human Resources ', 'Cairo'),
+       (3, 'Research & Development', 'Alexandria');
+
+INSERT INTO Staff.Employees
+VALUES (1, 'Shahd', 'Ammar', 5, '2020-6-15', 'Software Engineer', 50000),
+       (2, 'Ahmed', 'Ismail', 4, '2021-2-27', 'Production Manager', 65000),
+       (3, 'Marwan', 'Atef', 2, '2021-4-13', 'HR Director', 73000);
